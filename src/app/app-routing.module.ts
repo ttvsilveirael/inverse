c2f553a1ddeceff0e0a1d6675c90d7cd4d2ea26b
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2022-05-21 11:27:51 -0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PistaComponent } from './components/pista/pista.component';
+
+const routes: Routes = [
+  { path: 'corrida', component: PistaComponent },
+  { path: '', component: HomeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+}
---
+++ src/app/components/my_image/my_image.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-image',
+  templateUrl: './my_image.component.html',
+  styleUrls: ['./my_image.component.scss']
+})
+export class MyImageComponent {
+}
