b983be177a6909d081f3b7e61b544f4b8d53b581
Eduardo Silveira <eduardo.luiz@ubanquity.com>
2022-05-26 19:39:27 -0300

    add components to twitch

---
+++ src/app/components/my_twitch_history/my_twitch_history.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+declare var Twitch: any;
+
+@Component({
+    selector: 'twitch-history',
+    templateUrl: './twitch-history.component.html',
+    styleUrls: ['./twitch-history.component.scss']
+})
+export class TwitchHistoryComponent { }
